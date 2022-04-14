230654555e3dd9adb7c723e9c39eec7793b55c9a
AZEBELEV <92332567+AZEBELEV@users.noreply.github.com>
2022-04-14 14:10:33 +0300

    com

--- allExceptTimer/dist/buttons.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// // задача 1: создайте тип AnyButtonType, который описывает все вариации кнопок
-// type AnyButtonType = ...; // только без копипасты литералов, пожалуйста
-var button = {
-    type: 'Add',
-    action: 'Yes'
-};
